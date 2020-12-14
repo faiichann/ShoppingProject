d3a93c3cff82294072f543016fb23ee70b09fca7
Nichkamon Promputta <fainichkamon@gmail.com>
2020-12-15 05:24:05 +0700

    finish admin systtem

--- ShoppingProject/Controllers/ShopController.cs
+++ ShoppingProject/Controllers/ShopController.cs
@@ -3,17 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ShoppingProject.Models;
+using ShoppingProject.ViewModel;
+using System.IO;
 
 namespace ShoppingProject.Controllers
 {
+
     public class ShopController : Controller
     {
+        ShopDBEntities db = new ShopDBEntities();
         // GET: Shop
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<ListModel> listStore = (from e in db.tbItems
+                                                join s in db.tbTypes
+                                                on e.Type_Id equals s.Type_Id
+                                                select new ListModel()
+                                                {
+                                                    Li_Id = e.It_Id,
+                                                    Li_Img = e.It_Img,
+                                                    Type = s.Type_Name,
+                                                    li_Code = e.It_Code,
+                                                    Li_Name = e.It_Name,
+                                                    Li_Des = e.It_Des,
+                                                    Li_Price = (decimal)e.It_Price,
+                                                    Li_Unit = (int)e.It_Unit
+                                                }).ToList();
+
+            return View(listStore);
         }
-        public ActionResult Order()
+            public ActionResult Order()
         {
             return View();
         }
---
+++ ShoppingProject/ViewModel/ListModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShoppingProject.ViewModel
+{
+    public class ListModel
+    {
+        public Guid Li_Id { get; set; }
+        public string Type { get; set; }
+        public string li_Code { get; set; }
+        public string Li_Name { get; set; }
+        public string Li_Des { get; set; }
+        public string Li_Img { get; set; }
+        public decimal Li_Price { get; set; }
+        public decimal Li_Unit { get; set; }
+
+    }
+}
