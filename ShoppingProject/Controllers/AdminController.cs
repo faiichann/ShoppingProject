2dabf00706e5aee3e5b587e0c3f139bb9585968d
noojob001 <noojob001@outlook.com>
2020-12-15 22:11:22 +0700

    up

---
+++ ShoppingProject/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShoppingProject.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult DataVisual()
+        {
+            return View();
+        }
+    }
+}
